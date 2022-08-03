8a5d902918e180ef9118fa68ef155f16c0b826b4
BOTz44 <106071317+BOTz44@users.noreply.github.com>
2022-08-03 20:34:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6282142185506','62895336722950','6287721317870','6288292024190']
+global.premium = ['6282142185506']
+global.packname = 'Yui-Chan'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
